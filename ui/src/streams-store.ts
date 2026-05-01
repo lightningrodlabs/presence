67a01dd4c3ffd56196b7e1712308f7ac7a6785ec
Eric Harris-Braun <eric@harris-braun.com>
2026-05-01 15:02:29 -0400

    fix: add more stun server

--- ui/src/streams-store.ts
+++ ui/src/streams-store.ts
@@ -55,7 +55,14 @@ declare const __APP_VERSION__: string;
 
 const STUN_SERVERS: RTCIceServer[] = [
   { urls: 'stun:global.stun.twilio.com:3478' },
-  { urls: 'stun:stun.l.google.com:19302' },
+  { urls: 'stun:stun.cloudflare.com:3478' },
+  {
+    urls: [
+      'stun:stun.l.google.com:19302',
+      'stun:stun1.l.google.com:19302',
+      'stun:stun2.l.google.com:19302',
+    ],
+  },
 ];
 
 /**
